d5048fc2e100dc74316994107399e3db298d99c3
vanderkyl <vandermail@utexas.edu>
2014-03-25 13:54:23 -0500

    Update PlayGround.h

--- PlayGround.h
+++ PlayGround.h
@@ -1,4 +1,3 @@
-
 #ifndef __PlayGround_h_
 #define __PlayGround_h_
 
@@ -31,8 +30,8 @@ struct MyPlaneContactResultCallback : public btCollisionWorld::ContactResultCall
     MyPlaneContactResultCallback(void) : hit(false){}
 
     virtual btScalar addSingleResult(btManifoldPoint& cp,
-	const btCollisionObjectWrapper* colObj0, int partId0, int index0,
-	const btCollisionObjectWrapper* colObj1, int partId1, int index)
+	const btCollisionObject* colObj0, int partId0, int index0,
+	const btCollisionObject* colObj1, int partId1, int index)
     {
 	hit = true;
     }
